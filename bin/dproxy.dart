248fd2d49d109a926d7aecd19acd261428110c23
ilshookim <ilshookim@gmail.com>
2021-06-22 23:49:30 +0900

    + connection with sid

--- bin/dproxy.dart
+++ bin/dproxy.dart
@@ -3,19 +3,28 @@ import 'package:shelf_web_socket/shelf_web_socket.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() {
+  Map<WebSocketChannel, String> connections = Map();
+
   var handler = webSocketHandler((WebSocketChannel ws) {
-    print('connected');
+    final DateTime now = DateTime.now();
+    final String sid = now.toIso8601String();
+    connections[ws] = sid;
+    print('connected: sid=$sid, connections=${connections.length}');
+
     ws.stream.listen((message) {
-      ws.sink.add("echo $message");
-      print('echo $message');
+      final String id = connections[ws] ?? '(nothing)';
+      final Map echo = { 'sid': id, 'message': message };
+      ws.sink.add("$echo");
+      print('echo: sid=$id, message=$message');
     }, onDone: () {
-      print('done');
-    }, onError: (err) {
-      print('error=$err');
+      print('close: sid=$sid, connections=${connections.length}');
+    }, onError: (error) {
+      print('error: sid=$sid, connections=${connections.length}, error=$error');
     });
+
   });
 
-  shelf_io.serve(handler, 'localhost', 8080).then((server) {
+  shelf_io.serve(handler, 'localhost', 9450).then((server) {
     print('Serving at ws://${server.address.host}:${server.port}');
   });
 }
