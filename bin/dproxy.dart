45388dd1eeab8043c681779cb31fe87f55f5897e
ilshookim <ilshookim@gmail.com>
2021-06-23 00:20:37 +0900

    + connections in print

--- bin/dproxy.dart
+++ bin/dproxy.dart
@@ -3,23 +3,25 @@ import 'package:shelf_web_socket/shelf_web_socket.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() {
-  Map<WebSocketChannel, String> connections = Map();
+  Map<WebSocketChannel, String> _connections = Map();
 
   var handler = webSocketHandler((WebSocketChannel ws) {
     final DateTime now = DateTime.now();
     final String sid = now.toIso8601String();
-    connections[ws] = sid;
-    print('connected: sid=$sid, connections=${connections.length}');
+    _connections[ws] = sid;
+    print('connections=${_connections.length}, sid=$sid');
 
     ws.stream.listen((message) {
-      final String id = connections[ws] ?? '(nothing)';
-      final Map echo = { 'sid': id, 'message': message };
+      final String id = _connections[ws] ?? '(nothing)';
+      final Map echo = { 'sid': id, 'msg': message };
       ws.sink.add("$echo");
-      print('echo: sid=$id, message=$message');
+      print('echo: sid=$id, msg=$message');
     }, onDone: () {
-      print('close: sid=$sid, connections=${connections.length}');
+      _connections.remove(ws);
+      print('close: sid=$sid, connections=${_connections.length}');
     }, onError: (error) {
-      print('error: sid=$sid, connections=${connections.length}, error=$error');
+      _connections.remove(ws);
+      print('error: sid=$sid, connections=${_connections.length}, error=$error');
     });
 
   });
